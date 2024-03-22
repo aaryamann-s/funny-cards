24031502237ab48d897e4ff9ff3aa7c07f8a7902
Aaryamann Singh <aaryamannsingh3@gmail.com>
2024-03-22 15:53:50 +0530

    Project Initialization

---
+++ apps/funny-cards-go-brr-server/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'funny-cards-go-brr-server',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/funny-cards-go-brr-server',
+};
